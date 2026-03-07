196c774db40c63a95dda96d541d94c61088d0f04
Abderrafia <abderafia@gmail.com>
2026-03-07 13:43:00 +0100

    fixing map in the contact page

--- components/sections/SchoolMap.tsx
+++ components/sections/SchoolMap.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
+import { useEffect } from 'react'
 
 // Fix Leaflet default marker icon in Next.js
-// We need to do this because the default icon paths are broken by build systems
 const icon = L.icon({
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
@@ -16,16 +15,29 @@ const icon = L.icon({
   shadowSize: [41, 41],
 })
 
-const SCHOOL_COORDS: [number, number] = [51.35787, 7.46467] // Wehringhausener Str. 1, Hagen
+const SCHOOL_COORDS: [number, number] = [51.35787, 7.46467]
+
+function ChangeView() {
+  const map = useMap()
+  useEffect(() => {
+    // Force invalidating size to fix gray tiles issue
+    setTimeout(() => {
+      map.invalidateSize()
+    }, 100)
+  }, [map])
+  return null
+}
 
 export default function SchoolMap() {
   return (
     <MapContainer
       center={SCHOOL_COORDS}
       zoom={16}
-      className="h-64 w-full rounded-xl"
+      className="h-64 w-full rounded-xl overflow-hidden"
+      style={{ height: '256px', width: '100%' }}
       scrollWheelZoom={false}
     >
+      <ChangeView />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
